a4cd7a07c18f767c9f7cadeff74a34b51736026b
eu-maria12 <mariafernandacvo12@gmail.com>
2025-10-06 21:44:02 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,17 @@
 import java.util.Scanner;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /*
+      for(int i=0; i<=50; i++){
+          System.out.println(i);
+      }
+      */
 
-        Scanner input = new Scanner(System.in);
-
-    System.out.print("Digite o consumo em km/h: ");
-    int consumo = input.nextInt();
-
-    double valorConta;
-
-    // Definindo o preço por faixa
-        if (consumo <= 100) {
-            valorConta = consumo * 0.50;
-        } else if (consumo <= 200) {
-            valorConta = consumo * 0.70;
-        } else {
-            valorConta = consumo * 0.90;
+        for (int i = 50; i >= 0; i--) {
+            System.out.println(i);
         }
-
-        // Regras adicionais
-
-        if (consumo > 500) {
-            System.out.println("Consumo elevado: considere economizar energia");
-        }
-
-        if (valorConta > 200) {
-            valorConta = valorConta * 0.90; // aplica 10% de desconto
-        }
-
-        if (valorConta < 30) {
-            System.out.println("Valor nãp será cobrado neste mês. Será somado à próxima conta.");
-        } else {
-            System.out.println("Valor da conta: R$ " + valorConta);
-        }
-
-
     }
 }
